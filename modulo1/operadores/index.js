c4ceb4d12a04b5eacad2c524ae9b4d66bb6e18eb
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2021-11-15 07:24:21 -0300

    alterações

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,42 @@
+
+//Exercício 1
+
+const idadeDoUsuario = Number (prompt ("Qual a sua idade?"))
+const idadeDaJasmim = Number (prompt ("Qual é a sua idade, Jasmim?"))
+
+console.log ("idadeDoUsuario" ,"idadeDaJasmim") 
+console.log ("idadeDoUsuario:" , "idadeDaJasmim" >= 18)
+
+console.log(idadeDoUsuario >= idadeDaJasmim)
+
+console.log(idadeDaJasmim - idadeDoUsuario)
+
+
+//Exercício 2
+
+
+const numeroDoUsuario = Number (prompt ("Insira o número par?"))
+console.log (numeroDoUsuario % 2 )
+
+//Quando eu insiro um número par, sendo divido por 2 ele me dá o resultado de 0
+//Quando divido por um número ímpar, ele me da um resultado de 1
+
+//Exercício 3
+
+let idadeEmAnos = Number(prompt ("Qual a sua idade?"))
+let idadeEmMeses = idadeEmAnos * 12
+let idadeEmDias = idadeEmAnos * 365
+let idadeEmHoras = idadeEmAnos * 24
+
+console.log (idadeEmMeses) , (idadeEmDias) , (idadeEmHoras)
+
+
+//Exercício 4
+
+let primeiroNumero = Number (prompt ("Insira um número"))
+let segundoNumero = Number (prompt ("Insira 2 segundo número"))
+
+console.log ("o primeiro número é maior que o segundo? $ {primeiroNumero > segundoNumero}")
+
+// não consegui sair dessa parte.
+// Tentei fazer o exercício sozinha assim para treinar mesmo, pedi ajuda, mas preciso bater a cabeça e aprendar resolver.
