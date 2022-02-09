fc943b08785acd8cfc50ce56bc28eb8b49c52439
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-02-08 21:52:53 -0300

    Exercício de renderização

---
+++ modulo2/rend-condicional-form/condicional/src/components/Etapa1.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Etapa1() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default Etapa1;
---
+++ modulo2/rend-condicional-form/condicional/src/components/Etapa2.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Etapa2() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default Etapa2;
