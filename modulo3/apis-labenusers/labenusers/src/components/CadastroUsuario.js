7129eee5a8d3abb69c3d556aa523fcdec05d39a2
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-02-24 21:32:13 -0300

    Exercício labenusers

---
+++ modulo3/apis-labenusers/labenusers/src/components/CadastroUsuario.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+export default class CadastroUsuario extends React.Component{
+ render() {
+  return (
+        <div>
+
+    <div>
+       <input placeholder='Nome'/>
+       <input placeholder='E-mail'/>
+       <button> Criar usuário</button>
+    </div>
+
+</div>
+
+ )
+ }
+ }
+
---
+++ modulo3/apis-labenusers/labenusers/src/components/ListaUsuario.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+import React from "react"
+import styled from " style-components"
+
+
