48024b6c89a4c1e671d74bf806fdd051311ee2d2
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-03-05 20:36:52 -0300

    Criação de playlist,renderização na tela e exclusão

--- modulo3/projeto-labefy/labefy/src/App.js
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -1,42 +1,177 @@
-import styled from "styled-components";
-import axios from 'axios'
+import styled from 'styled-components';
+import axios from 'axios';
 import React from 'react';
 
+const Header =styled.header `
+ background-color: #39752D;
+ display: flex;
+ justify-content: center;
+ color: white;
+ font-size: 30px;
+ margin: 0px;
+ 
 
-export default function App() {
-
-
-const header = styled.div`
+`
+const Layout = styled.div`
 display: flex;
-align-items:center;
-font-size: 14px;
+flex-direction: column;
+width: 100vw;
+background-color:#BDD5B8;
+
+
+criacaoPlaylist {
+ display: flex; 
+ flex-direction: column;
+ align-items: center;
+ width:200px;
+ background-color:#82C175;
+ color: white;
+ margin: 100px;
+ padding: 50px;
+ font-size: 25px;
+ font-family:Arial;
+ font-style:italic;
+}
+
+button {
+  margin: 15px;
+  padding:10px;
+  font-size: 15px;
+  font-weight: bold;  
+}
+
+
+
+playlistCriada { 
+display: flex; 
+width:150px;
+height: 400px;
+background-color:#82C175;
+color: white;
+margin: 500px;
+padding: 80px;
+margin-top: 0px;
+font-family:Arial;
+font-style:italic;
+font-size: 20px;
+}
 
 `
 
-const adicionaPlaylist=() => {
-  axios.post ('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',{
-    headers: {
-      Authorization: "maria-freitas-turmaGuimaraes"
 
-    }  
+class App extends React.Component {
+state ={
+playlists: [],
+inputCriar: "" 
+
+}
+
+componentDidMount () {
+ this.getAllPlaylists ()
+
+}
+
+getAllPlaylists = () => {
+ axios
+ .get ('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+  {
+   headers: {
+    Authorization: 'maria-freitas-turmaGuimaraes'
+   }
+ }).then ((response) => {
+   this.setState ({playlists:response.data.result.list})
+   console.log (response.data.result.list)
+
+ }).catch ((error) => console.log (error))
+ 
+} 
+
+manuseioDoInput = (event) => {
+ this.setState ({inputCriar: event.target.value})
+ 
+}
+
+createPlaylist = () => {
+ const body ={
+ name:this.state.inputCriar
+ }
+
+ axios.post ('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+ body, {
+   headers: {
+     Authorization: 'maria-freitas-turmaGuimaraes'
+    }
+  }).then ((response)=> {
+   console.log (response.data) 
+
+  }).catch ((error) => {
+    console.log (error.response.data)
   })
+    
+}
 
-  // {
-  //   "name"= "Adiciona sua musica";
-  // }
+deletePlaylist = (idDaPlaylist) => {
+ axios
+ .delete (`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idDaPlaylist}`,
+ {
+  headers: {
+   Authorization: 'maria-freitas-turmaGuimaraes'
+   }
+  }).then((response) => {
+    this.getAllPlaylists ()
+    console.log (response)
+  }).catch ((error) => 
+    console.log (error.response))
+
+}
+
+  render() {
+   const renderizaPlaylists = this.state.playlists.map((playlist) => {
+   return ( 
+    <div>
+      <p key ={playlist.id} > {playlist.name}</p>
+      <button onClick={() => this.deletePlaylist(playlist.id)}>excluir playlist</button>
+    </div>
+   )
+
+   })
   
+    
+   return (
+
+ <div>
+   <Layout>
+
+      <Header> 
+        <p>Labefy</p>
+      </Header>
+      
+      <criacaoPlaylist> 
+        <p><u>Crie sua Playlist</u></p>
+          <input placeholder="Diga aí sua playlist" value ={this.state.inputCriar} 
+          onChange= {this.manuseioDoInput}/>
+          <button onClick={this.createPlaylist}>adicionar</button>
+      </criacaoPlaylist> 
+      
+      <playlistCriada>
+        <p><u>Lista de Playlists</u></p>
+          {renderizaPlaylists}
+      </playlistCriada>
+
+
+      <p>Adicionar música</p>
+
+   </Layout>
+ </div>
+
+
+
+   )
+
+
+  }
+
+
 }
 
-
-
-
-  return (
-    <header>
-      <h1>Labefy</h1>
-      <input type="text" placeholder='Diz aí sua playlist?'/>
-      <button onClick={adicionaPlaylist}>Adicione sua música</button> 
-    </header>
-  );
-}
-
-
+export default App;
