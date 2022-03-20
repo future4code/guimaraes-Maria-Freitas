29aa5f559603a1bcc22da26658312a9f9f2bff2b
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-03-20 12:44:24 -0300

    Início do Projeto

---
+++ modulo4/astromatch/astromatch/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { TelaPrincipal } from "./Components/TelaPrincipal";
+import { TelaMatch } from "./Components/TelaMatch";
+import './App.css';
+import axios from "axios";
+
+
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+    <TelaPrincipal/>
+   
+    
+   
+    </div>
+  );
+}
+
+
---
+++ modulo4/astromatch/astromatch/src/Components/TelaPrincipal.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export function TelaPrincipal () {
+
+
+return (
+  <div>
+    
+    <header>
+        <h3>Astromatch</h3>
+        <button>X</button>
+        <button>♥️</button>
+
+    </header>  
+   
+
+
+  </div>
+ )
+
+}
+
