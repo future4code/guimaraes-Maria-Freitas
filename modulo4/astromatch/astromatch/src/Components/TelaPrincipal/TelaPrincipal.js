0fb04797a741bd49e932181a3bc2725c8621cfec
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-03-23 21:36:48 -0300

    Quarta-feira

--- modulo4/astromatch/astromatch/src/Components/TelaPrincipal/TelaPrincipal.js
+++ modulo4/astromatch/astromatch/src/Components/TelaPrincipal/TelaPrincipal.js
@@ -1,24 +1,28 @@
 import React from "react"
 import styled from "styled-components"
+import axios from 'axios'
 
 const AppContainer =styled.div `
-border: 1px solid #8B8989;
+border: 2px solid red;
 height: 70vh;
 width: 300px;
 margin: auto;
 margin-top: 30px;
 background-color: #EEE5DE;
-display: grid;
-grid-template-rows: 1fr 40fr 290fr;
-grid-auto-rows: 70px;
 
+`
+
+const AppHeader =styled.div `
+display: flex;
+/* flex-direction: row; */
+justify-content: space-around;
 
 
 
 h3 {
  margin: 5px;
  color: red;
- padding-left: 90px;
+ padding-left: 55px;
    
 }
 
@@ -31,9 +35,7 @@ button {
  font-size: 20px;
  border: 1px solid red;
 
- 
 }
-
 
 `
 const AppMain = styled.div `
@@ -45,6 +47,7 @@ const AppMain = styled.div `
 const AppFooter = styled.div `
  display: flex;
  justify-content: space-evenly;
+ 
  
  
 
@@ -67,35 +70,56 @@ button{
 
 export function TelaPrincipal () {
   
-const executaMatch = () => {
+const executaMatch = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person"
 
 
+const profile = [
+  {
+    id: "xUrxMGvODWZa4ZASbfwx",
+    age: 26,
+    name: "Carol Danvers",
+    photo: "https://s1.r29static.com/bin/entry/7e8/340x408,85/2138124/image.webp",
+    bio: "Gosto de voar e de gatos. Procuro relações que respeitem minha independência."
+ },
+ {
 
+    id: "71gMbZs2txS9LDvGK5Ew",
+    age: 26,
+    name: "Anitta",
+    photo: "https://images.outgo.com.br/clients/1/events/2923/Anitta-Perfil-4-1.png",
+    bio: "Amo cachorros e sair para dançar. Procuro alguém animado e sem neuras."
+},
+
+]
+
+const ProfileToChoose = () => {
+ axios
+ .get (executaMatch,profile)
+ .then (()=> {})
 }
 
 
 
 
+
 return (
-    <div>
-          
-        <AppContainer>
+    <AppContainer>
+                
+         <AppHeader>
             <h3>AstroMatch</h3>
             <button>♥️</button>
-
+         </AppHeader>
+{/* 
           <AppMain>
-           <p>teste</p> 
-          </AppMain>
+            <p>teste</p> 
+          </AppMain> */}
 
           <AppFooter>
-            <button onClick={executaMatch}>♥️</button>
-            <button>X</button>
-          </AppFooter>
-          
-      </AppContainer>
-
-         
-    </div>
+            {/* <button class="sc-gZMcBi fRXBMM">♥️</button>
+            <button class="sc-gZMcBi lbldej">X</button> */}
+          </AppFooter> 
+               
+    </AppContainer>
  )
 
 }
