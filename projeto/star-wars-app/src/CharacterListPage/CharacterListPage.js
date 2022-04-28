f13e2c8036a03073cf8394a08e8e7144418a6337
Maria Karolina <mar_krol@hotmail.com[A[A[A[A>
2022-04-27 21:11:07 -0300

    Parte 2 - Renderizãção de Personagens

--- projeto/star-wars-app/src/CharacterListPage/CharacterListPage.js
+++ projeto/star-wars-app/src/CharacterListPage/CharacterListPage.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react"
+import axios from "axios"
 import {useNavigate} from 'react-router-dom'
+import { BASE_URL } from "../constants/urls"
+import {useEffect, useState} from 'react'
 
+
+const [characterList, setCharacterList] =useState([])
 
 const CharacterListPage= () => {
-  const navigate = useNavigate ()
+const navigate = useNavigate ()
+
+const goToDetailPage= () => {
+  navigate ('/CharacterDetailPage')
+ }
    
-    const goToDetailPage= () => {
-     navigate ('/CharacterDetailPage')
-    }
+  const getCharacterList= () => {
+    axios.get= (`${BASE_URL}/people`)
+    .then ((res) => console.log (res))
+    .catch((err) => console.log (err)) 
+  
+  }
+
+  const listPage = () => {
+   setCharacterList (characterList)
+
+  }
+
+  
+  useEffect (()=> {
+    getCharacterList ()
+  
+  },[])
+  
 
   return (
     <div>
       <h1>Lista Page Star Wars</h1>
       <button onClick ={goToDetailPage}>Ir para Detalhes</button>
+      {characterList}
+      {listPage}
     </div>
   )
 }
---
+++ projeto/star-wars-app/src/constants/urls.js
@@ -0,0 +1,5 @@
+export const BASE_URL= "https://swapi.dev/api"
+
+
+// essa uls é colocado numa pasta, pois assim chamo em qualquer lugar, passando /o que quero.
+// Documentação
