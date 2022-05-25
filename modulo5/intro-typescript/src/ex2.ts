22097b624610d25858fbc5c6bfccc5cf63b5fd36
maria-freitas <mar_krol@hotmail.com>
2022-05-25 18:53:43 -0300

    Terminando o exercicio

---
+++ modulo5/intro-typescript/src/ex2.ts
@@ -0,0 +1,14 @@
+//Exercício 2
+
+    function imprimeTresCoresFavoritas():void {
+    const cor1: string = prompt("Insira sua primeira cor favorita")
+    const cor2: string = prompt("Insira sua segunda cor favorita")
+    const cor3: string= prompt("Insira sua terceira cor favorita")
+    let array: string [] = [cor1 , cor2 , cor3]
+    
+ }
+   console.log(imprimeTresCoresFavoritas("azul", "amarelo", "verde"))
+
+
+   //tirar dúvida com a Jú.
+
