d61ce3af387db7ec940e831fe499050be8cb785b
maria-freitas <mar_krol@hotmail.com>
2022-05-26 16:08:35 -0300

    Exercicio pela metade

---
+++ modulo5/aprofundamento-typescript/src/ex1.ts
@@ -0,0 +1,19 @@
+//Exercício 1 a)
+
+// let minhaString: string = 5
+
+// Ele não aceita, pois estou tipando uma string ou seria um number.
+
+
+//Exercício 1 b)
+
+// let meuNumero: number = 10
+// let meuNumero: number | string | undefined
+
+//Exercício 1 c)
+
+type person={
+ name: string,
+ idade: number,
+corFavorita: string
+}
