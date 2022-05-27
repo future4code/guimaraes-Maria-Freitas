717ee8d23c5852b42b686770e880dbb5ac8c624c
maria-freitas <mar_krol@hotmail.com>
2022-05-27 19:23:22 -0300

    Início Projeto

---
+++ modulo5/lista-typescript/src/ex1.ts
@@ -0,0 +1,8 @@
+const dadosPessoais =(nome:string ,data:string)=> {
+ const dados:string= "Maria nasci na data 20061988"
+//  const resultado:string=dados.split(".")
+ return dados.split(".")
+}
+console.log(dadosPessoais)
+
+// "Olá me chamo {NOME}, nasci no dia {DIA} do mês de {MÊS} do ano de {ANO}" 
