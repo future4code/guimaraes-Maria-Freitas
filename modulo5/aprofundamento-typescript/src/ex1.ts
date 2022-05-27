77732ea88db188a79be1fdf8f19a935511a19395
maria-freitas <mar_krol@hotmail.com>
2022-05-26 22:31:05 -0300

    Exercício Aprofundamento Typescript

--- modulo5/aprofundamento-typescript/src/ex1.ts
+++ modulo5/aprofundamento-typescript/src/ex1.ts
@@ -1,5 +1,7 @@
 //Exercício 1 a)
 
+import { isMethodDeclaration } from "typescript";
+
 // let minhaString: string = 5
 
 // Ele não aceita, pois estou tipando uma string ou seria um number.
@@ -7,13 +9,33 @@
 
 //Exercício 1 b)
 
-// let meuNumero: number = 10
+// let meuNumero: number =10,
 // let meuNumero: number | string | undefined
 
 //Exercício 1 c)
 
-type person={
- name: string,
+type Person={
+ nome: string,
  idade: number,
-corFavorita: string
+ corFavorita:string
+
 }
+
+const Pessoa:Person ={
+ nome:"Maria",
+ idade:33,
+ corFavorita: "verde"
+}
+console.log(Pessoa)
+
+enum LabenuClass {
+MARIA= "Maria",
+IDADE= 33,
+VERDE= "Verde"
+}
+
+const coresFavoritas={
+class:LabenuClass.VERDE
+}
+
+console.log(coresFavoritas)
