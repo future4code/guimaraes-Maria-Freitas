473b838e539a674f4f73dc9e2568b5b52fc5e3b2
maria-freitas <mar_krol@hotmail.com>
2022-06-02 15:41:09 -0300

    Refazendo exercícios

--- modulo5/intro-typescript/src/ex2.ts
+++ modulo5/intro-typescript/src/ex2.ts
@@ -1,14 +1,12 @@
 //Exercício 2
 
-    function imprimeTresCoresFavoritas(a:string , b:string , c:string):void {
-    const cor1: string = prompt("Insira sua primeira cor favorita")
-    const cor2: string = prompt("Insira sua segunda cor favorita")
-    const cor3: string= prompt("Insira sua terceira cor favorita")
-    let array: string [] = [cor1 , cor2 , cor3]
+    function imprimeTresCoresFavoritas(cor1:string , cor2:string , cor3:string):string[] {
+    let array= [] 
     
+    array.push(cor1,cor2,cor3)
+    return array
  }
    console.log(imprimeTresCoresFavoritas("azul", "amarelo", "verde"))
 
 
-   //tirar dúvida com a Jú.
-
+  
---
+++ modulo5/intro-typescript/src/ex7.ts
@@ -0,0 +1,2 @@
+// Escreva um programa que converta uma string de DNA em uma string de RNA. Para os exemplos acima, a entrada seria
+//  "ATTGCTGCGCATTAACGACGCGTA" e a saída "UAACGACGCGUAAUUGCUGCGCAU
