726c5dc6b381ed240f71cb155d6f604735cfc354
maria-freitas <mar_krol@hotmail.com>
2022-06-14 18:43:48 -0300

    Projeto em andamento

--- modulo5/sistema-bancario/src/index.ts
+++ modulo5/sistema-bancario/src/index.ts
@@ -1,6 +1,10 @@
 import express, {Express} from 'express'
 import cors from 'cors'
 import { AddressInfo } from "net";
+import { Cliente, usuario } from './data';
+import { setOriginalNode } from 'typescript';
+
+
 
 const app: Express = express();
 
@@ -8,6 +12,38 @@ app.use(express.json());
 app.use(cors());
 
 
+
+// Criação novo usuário - método post
+
+
+app.post("/user",(req, res)=>{
+  try {
+    const name= req.body.name
+    const documento= req.body.cpf
+    const data= req.body.data
+        
+   const newUsers:Cliente={
+     nome: name,
+     cpf: documento,
+     dataNascimento:data,
+     saldo:0,
+     extrato: []
+   }
+   usuario.push(newUsers)
+   res.status(200).send(usuario)
+
+  } catch (error:any) {
+    res.status(400).send(error.message)
+  }
+
+})
+
+
+// Pegar os usuários
+
+app.get('/users', (req, res) => {
+    res.status(200).send(usuario)
+})
 
 
 
