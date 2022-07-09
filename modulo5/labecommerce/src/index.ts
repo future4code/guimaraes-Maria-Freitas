82a1d5f8020cab7f99874fde87ce6f0507a0abc8
maria-freitas <mar_krol@hotmail.com>
2022-07-09 10:21:39 -0300

    Continuação do projeto

--- modulo5/labecommerce/src/index.ts
+++ modulo5/labecommerce/src/index.ts
@@ -2,24 +2,80 @@ import express from "express";
 import cors from "cors"
 import { AddressInfo } from "net";
 import { Request,Response } from "express";
+import connection from "./connection";
+import {v4 as generateID} from "uuid"
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.post("/users",(req:Request , res:Response)=>{
-try {
+
+app.post("/users", async(req:Request , res:Response)=>{
+  try {
+      
+    await connection ("labecommerce_users")
+     .insert ({
+      id: generateID(),
+      nome: req.body.nome,
+      email: req.body.email,
+      password: req.body.password
+     })
+    res.status(200).send("Usuário cadastrado")
+   
+  } catch (error) {
+      res.status(400).send("Usuário não cadastrado")
+  }
+  
+  
+  })
+
+  app.get ("/users", async(req:Request , res:Response)=>{
+  try {
+    const response= await connection("labecommerce_users")
+    .select ("*")
+
+    res.status(200).send(response)
     
-  const {name , email , password } = req.body
+  } catch (error) {
+    res.status(400).send("Lista não retornada")
+  }
+
+  })
+
+  app.post ("/products", async(req:Request , res:Response)=>{
+   try {
+    
+    await connection ("labecommerce_products")
+    .insert ({
+      id: generateID(),
+      nome: req.body.nome,
+      price: req.body.price,
+      image_url: req.body.image_url
+    })
+    res.status(200).send("Produto cadastrado")
+
+   } catch (error) {
+    res.status(400).send("Produto não cadastrado")
+   }
+
+  })
+
+  app.get ("/products", async(req:Request , res:Response)=>{
+    try {
+      const response= await connection("labecommerce_products")
+      .select ("*")
+  
+      res.status(200).send(response)
+      
+    } catch (error) {
+      res.status(400).send("Produto não encontrado")
+    }
+  
+    })
 
 
-} catch (error) {
-    res.status(400).send("Usuário não cadastrado")
-}
-
-
-})
 
 
 
