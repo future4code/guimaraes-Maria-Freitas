87c318bb88fc92b78a710cda0f38f1cf3f58342a
maria-freitas <mar_krol@hotmail.com>
2022-08-23 20:30:48 -0300

    Exercício user-roles

---
+++ modulo5/user-roles/src/model/user.ts
@@ -0,0 +1,27 @@
+export type user = {
+   id: string
+   email: string
+   password: string
+   name: string
+   nickname: string
+}
+
+export interface UserInputDTO {
+   name: string,
+   nickname: string,
+   email: string,
+   password: string,
+   role:string
+}
+
+export interface EditUserInputDTO {
+   name: string,
+   nickname: string,
+   id: string
+}
+
+export interface EditUserInput {
+   name: string,
+   nickname: string,
+   id: string
+}
